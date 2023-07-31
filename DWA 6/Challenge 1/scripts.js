87b30f453133b06c10c0ecff71984550724c9a5f
Reniercoetzer92 <reniercoetzer92@gmail.com>
2023-07-31 16:00:29 +0200

    added book connect files for Challenge 1, added/modified Notes.txt

---
+++ DWA 6/Challenge 1/scripts.js
@@ -0,0 +1,25 @@
+import { BOOKS_PER_PAGE, books } from './data.js'
+import { html } from './queryselectors.js'
+import { updateShowMoreButton, populateBookItems, populatDropdownSearchGenres, populateDropdownSearchAuthors } from './view.js'
+import { toggleListDialog, showMoreBooks, toggleSearchDialog, toggleSettingsDialog, handleSearchFormSubmit, handleSettingFormSubmit } from './view.js'
+
+//if (!books && !Array.isArray(books)) throw new Error('Source required') 
+
+//Functions
+populateBookItems(books, 0, BOOKS_PER_PAGE)
+populatDropdownSearchGenres()
+populateDropdownSearchAuthors()
+updateShowMoreButton(books)
+
+//set initial theme selected value
+html.settings.theme.value = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day';
+
+//event listeners
+html.search.cancel.addEventListener('click', toggleSearchDialog)
+html.search.form.addEventListener('submit', handleSearchFormSubmit)
+html.list.close.addEventListener('click', toggleListDialog)
+html.list.button.addEventListener('click', showMoreBooks)
+html.header.search.addEventListener('click', toggleSearchDialog)
+html.header.settings.addEventListener('click', toggleSettingsDialog)
+html.settings.cancel.addEventListener('click', toggleSettingsDialog)
+html.settings.form.addEventListener('submit', handleSettingFormSubmit)
